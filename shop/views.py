1f91fe68de86956f20d0d7cd87ea962682ef0638
VishalSharma2001 <vishalsharma9785812846@gmail.com>
2021-04-19 17:53:49 +0530

    Add all my Files

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.shortcuts import render,HttpResponse
 from django.http import HttpRequest
+from shop.models import Product
 # Create your views here.
+from math import ceil
 
 def index(request):
-    return render(request,'index.html')
+    products=Product.objects.all()
+    n=len(products)
+    nSlides=n//4+ceil((n/4)+(n//4))
+    params={"no_of_slides":nSlides,'range':range(1,nSlides),'product':products}
+    return render(request,'index.html',params)
 def about(request):
-    return HttpResponse("This is About Page")
+    return render(request,'about.html')
 def contact(request):
     return HttpResponse("This is contact Page")
 def tracker(request):
